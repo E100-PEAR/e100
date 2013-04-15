0072fdef24542c08c756d074829edbfe5f5fd809
Matthew Leibold <leiboldm@caen-vnc07.engin.umich.edu>
2013-04-15 13:51:57 -0400

    readded the script because I may still need it

--- images/videos/VidtoJpeg.m
+++ images/videos/VidtoJpeg.m
@@ -1,15 +1,19 @@
-outputFolder = 'c:/users/whatever';  % Change this!
-% Read in the movie.
-mov = aviread(TWoodsFront.avi);
-% Determine how many frames there are.
+outputFolder = 'TWoodsFrontFrames';
+mov = VideoReader('TWoodsFront.avi');
 numberOfFrames = size(mov, 2);
 for frame = 1 : numberOfFrames
-    % Extract the frame from the movie structure.
     thisFrame = mov(frame).cdata;
-    % Create a filename.
     outputBaseFileName = sprintf('Frame%4.4d.jpg', frame);
     outputFullFileName = fullfile(outputFolder, outputBaseFileName);
-    % Write it out to disk.
     imwrite(thisFrame, outputFullFileName, 'jpg');
 end
 
+outputFolder = 'TWoodsBehindFrames';
+mov = VideoReader('TWoodsBehind.avi');
+numberOfFrames = size(mov, 2);
+for frame = 1 : numberOfFrames
+    thisFrame = mov(frame).cdata;
+    outputBaseFileName = sprintf('Frame%4.4d.jpg', frame);
+    outputFullFileName = fullfile(outputFolder, outputBaseFileName);
+    imwrite(thisFrame, outputFullFileName, 'jpg');
+end
